da97935f7e353716b955c56beb00212fe8100fc8
lavitramaheshwari <lavitra.maheshwari1308@gmail.com>
2026-08-18 16:02:34 +0000

    Website prototype

--- index.js
+++ index.js
@@ -6,39 +6,23 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+const port = 3000;
 
-//Middleware`
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render('index');
+const pages = ['/', '/speakers', '/team', '/registration'];
+
+pages.forEach((route) => {
+    app.get(route, (req, res) => {
+        const view = route === '/' ? 'index' : route.slice(1);
+        res.render(view);
+    });
 });
 
-app.get('/speakers', (req, res) => {
-    res.render('speakers');
-});
-
-app.get('/team', (req, res) => {
-    res.render('team');
-});
-
-app.get('/registration', (req, res) => {
-    res.render('registration');
-});
-/* 
-
-app.get('/login', (req, res) => {
-    res.render('gam');
-});
-
-app.get('/account', (req, res) => {
-    res.render('gam');
-}); */
-//Start Service
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
-    console.log("http://localhost:3000");
+app.listen(port, () => {
+    console.log('server is running on port ' + port);
+    console.log('http://localhost:' + port);
 });
