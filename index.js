f29eb0fe28faa89a0b91e72e47f8a22ac4b051af
lavitramaheshwari <lavitra.maheshwari1308@gmail.com>
2026-08-24 08:08:04 +0800

    Update index.js, fixing redirect issue

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
